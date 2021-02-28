d2280d9e49e57f5b7607a468f200bdd5cea7d822
Rafael Negron <r.negron1@umiami.edu>
2021-02-28 17:44:25 -0500

    Refactoring

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const express = require('express');
 const p = require('phin');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
@@ -29,35 +30,42 @@ const createWeatherClient = (() => {
     }
 })();
 
-app.post('/weather', (req, res) => {
+const isSuccessStatusCode = statusCode => statusCode >= 200 && statusCode <= 299;
+
+app.post('/weather', async (req, res) => {
     if (!req.body.city) {
-        res.status(404).json({ message: 'You must include a payload containing your city name. '});
+        res.status(404).json({ message: 'You must include a payload containing your city name.' });
         return;
     }
 
-    p(createWeatherClient.getCityConditions(req.body.city))
-        .then(locationInformation => {
-            if (locationInformation.statusCode === 503) {
-                res.status(503).json({ message: 'Request limit exceeded! Please try again later.' });
-                return;
-            }
-    
-            p(createWeatherClient.getCityConditions(locationInformation.body[0].Key))
-                .then(citiesInfo => {
-                    const cityInformation = citiesInfo.body[0];
+    const locationInformation = await p(createWeatherClient.getLocationKey(req.body.city));
+    if (!isSuccessStatusCode(locationInformation.statusCode)) {
+        res
+            .status(locationInformation.statusCode)
+            .json({ message: locationInformation.statusMessage });
+        return;
+    }
 
-                    res
-                        .status(200)
-                        .json({ 
-                            dayTime: cityInformation.IsDayTime ? 'Yes' : 'No',
-                            temperature: cityInformation.Temperature.Imperial.Value,
-                            weatherCondition: cityInformation.WeatherText
-                         });
-                })
-                .catch(error => res.status(404).json({ error: error }));
-           
-        })
-        .catch(error => res.status(404).json({ error: error }));
+    const locationKey = locationInformation.body[0].Key;
+    
+    const citiesInformation = await p(createWeatherClient.getCityConditions(locationKey));
+    if (!isSuccessStatusCode(citiesInformation.statusCode)) {
+        res
+            .status(locationInformation.statusCode)
+            .json({ message: locationInformation.statusMessage });
+        return;
+    }
+
+    const cityInformation = citiesInformation.body[0];
+    const { isDayTime, Temperature: { Imperial: { Value } }, WeatherText } = cityInformation;
+
+    res
+        .status(200)
+        .json({ 
+            dayTime: isDayTime ? 'Yes' : 'No',
+            temperature: Value,
+            weatherCondition: WeatherText
+        });
 });
 
 app.listen(3000, () => console.log('Listening...'));
